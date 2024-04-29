3e34d170a67783d34cdf52138fd01b77a5d0601c
sakshishrestha <sakshishrestha810@gmail.com>
2024-04-29 18:42:11 -0400

    Implemented Search Feature

---
+++ ILLMS/models/Book.cs
@@ -0,0 +1,18 @@
+﻿namespace ILLMS.models;
+
+public class Book
+{
+    public Book(string title, string author, string category, bool availability)
+    {
+        this.title = title;
+        this.author = author;
+        this.category = category;
+        this.availability = availability;
+    }
+
+    public string title { get; set; }
+    public string author { get; set; }
+    public string category { get; set; }
+    public bool availability { get; set; } 
+
+}
